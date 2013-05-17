f312ae8346057aad9cc413952a2a5de21c71687f
b02485 <mharding8@fullsail.edu>
2013-05-16 20:30:12 -0400

    First save after declaring variables and inputing name and.

--- Harding_Matthew_assignment2.js
+++ Harding_Matthew_assignment2.js
@@ -1,1 +1,11 @@
-alert("JavaScript works!");
+// Matthew Harding
+// SDI 1304
+// Project 2
+
+// Function of this program is to see if someone qualifies for a car loan
+// My variables
+
+canBorrowMoney = true, money = 50000, nissanAltima = "Good Car", toyotaCamry = "Nice Car",
+kiaOptima = "Excelent Car", person = "Matt", personP = "Matt\'s",
+carsToCheck = [nissanAltima, toyotaCamry, kiaOptima]
+
